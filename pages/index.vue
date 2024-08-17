58b99fa909e980b9bbf9b8330593f6d61853b4b7
tushar-me <tusharimran.dev@gmial.com>
2024-08-17 12:30:34 +0600

    Nuxt Multipurpose

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="py-10">
-        <a href="" class="flex items-center justify-center gap-2 text-xl">Tushar Imran <Icon name="uil:github" /></a>
+        <a href="https://github.com/tushar-me" target="_blank" class="flex items-center justify-center gap-2 text-xl">Tushar Imran <Icon name="uil:github" /></a>
     </div>
 </template>
